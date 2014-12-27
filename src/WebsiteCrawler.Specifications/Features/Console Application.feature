4faccc4792e046652f352abe4d15400b757026b6
Tim Murphy <tim@26tp.com>
2014-12-28 05:12:33 +1100

    Added 'Console Application' specification.

---
+++ src/WebsiteCrawler.Specifications/Features/Console Application.feature
@@ -0,0 +1,16 @@
+﻿Feature: Console Application
+	In order to test all links of a web site
+	As a web master
+	I want to specify a website to crawl
+
+Scenario: Run console application with valid arguments
+	When I run the console application with valid arguments
+	Then the web site is crawled
+	And the report lists all internal links
+	And the report lists all external links
+	And the report lists all broken links
+	And the report lists excluded urls
+
+Scenario: Run console application with invalid arguments
+	When I run the console application with invalid arguments
+	Then the application help is displayed
---
+++ src/WebsiteCrawler.Specifications/Steps/ConsoleApplicationSteps.cs
@@ -0,0 +1,56 @@
+﻿using TechTalk.SpecFlow;
+
+namespace WebsiteCrawler.Specifications.Steps
+{
+    [Binding]
+    public class ConsoleApplicationSteps
+    {
+        [When(@"I run the console application with valid arguments")]
+        public void WhenIRunTheConsoleApplicationWithValidArguments()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I run the console application with invalid arguments")]
+        public void WhenIRunTheConsoleApplicationWithInvalidArguments()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the web site is crawled")]
+        public void ThenTheWebSiteIsCrawled()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the report lists all internal links")]
+        public void ThenTheReportListsAllInternalLinks()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the report lists all external links")]
+        public void ThenTheReportListsAllExternalLinks()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the report lists all broken links")]
+        public void ThenTheReportListsAllBrokenLinks()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the report lists excluded urls")]
+        public void ThenTheReportListsExcludedUrls()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the application help is displayed")]
+        public void ThenTheApplicationHelpIsDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
