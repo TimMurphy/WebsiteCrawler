8b2c245f1ef9e5de0cb41671ce3c7cd3f4e6104c
Tim Murphy <tim@26tp.com>
2016-04-27 16:10:11 +1000

    Update script to use https://croquetscores.com as starting point

--- scripts/crawl-croquetscores.com.bat
+++ scripts/crawl-croquetscores.com.bat
@@ -2,7 +2,7 @@
 
 pushd ..\src\WebsiteCrawler.Console\bin\Debug
 
-WebsiteCrawler.exe --startUrl "http://www.croquetscores.com" --resultPath "%temp%\crawl-croquetscores.com.json" --waitAfterPageLoad 200
+WebsiteCrawler.exe --startUrl "https://croquetscores.com" --resultPath "%temp%\crawl-croquetscores.com.json" --waitAfterPageLoad 200
 
 if errorlevel 1 goto finally
 
